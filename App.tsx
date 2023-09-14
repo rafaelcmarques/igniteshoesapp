6dcb0e2bf7e0061897f5ff851f3b037f73b73206
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-09-14 18:15:15 -0300

    configuração inicial para implementar OneSignal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import  OneSignal  from 'react-native-onesignal';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
@@ -9,8 +10,11 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+OneSignal.setAppId("bdb1d805-de1f-4c86-a6a9-3f543a0c072e");
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
 
   return (
     <NativeBaseProvider theme={THEME}>
